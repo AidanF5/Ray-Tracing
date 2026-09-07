95bad8c9d323883b4c57f718a2608b0c2a188177
AidanF5 <aidanfarrell07@gmail.com>
2026-09-07 17:43:33 +0100

    Added disk and logic for intersection

---
+++ src/main/java/Disk.java
@@ -0,0 +1,28 @@
+public class Disk extends Object{
+    private Plane p;
+    private Vec3 centre;
+    float radius;
+
+    public Disk(float nx, float ny, float nz, float d,
+                float x, float y, float z,
+                float r, Vec3 c, Material mat){
+        p = new Plane(nx, ny, nz, d, c, mat);
+        this.mat = mat;
+        colour = c;
+        centre = new Vec3(x, y, z);
+        radius = r;
+    }
+
+    public RayHit getIntersection(Ray r, Interval interval){
+        RayHit hitty = p.getIntersection(r, interval);
+        if(!hitty.isValid()){
+            return hitty;
+        }
+        if(centre == new Vec3(0, 0, 0)){ //replace with a distance operation
+            //hit is in the disk
+            return hitty;
+        }
+        return new RayHit();
+
+    }
+}
