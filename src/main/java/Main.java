4c9d8e7be3b2528bee42e34485473c08401c36a7
AidanF5 <aidanfarrell07@gmail.com>
2026-09-12 21:15:11 +0100

    Created a new snowglobe scene

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ import org.lwjgl.system.MemoryUtil;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.lang.reflect.WildcardType;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.file.Files;
@@ -18,14 +17,17 @@ import java.io.File;
 public class Main {
     private static final int WIDTH = 1920;
     private static final int HEIGHT = 1080;
-    private static final int SAMPLES_PER_PIXEL = 2048;
-    private static final int BOUNCES_PER_RAY = 128;
+    private static final int SAMPLES_PER_PIXEL = 256;
+    private static final int BOUNCES_PER_RAY = 64;
 
-    private static final Vec3 camPos = new Vec3(13.0f, 2.0f, 3.0f);
-    private static final Vec3 camFor = VectorOperations.normalise(new Vec3(-13.0f, -2.0f, -3.0f));
+    //private static final Vec3 camPos = new Vec3(13.0f, 2.0f, 3.0f);
+    //private static final Vec3 camFor = VectorOperations.normalise(new Vec3(-13.0f, -2.0f, -3.0f));
+    //private static final Vec3 camUp = new Vec3(0.0f, 1.0f, 0.0f);
+    private static final Vec3 camPos = new Vec3(0, 2f, -0.2f);
+    private static final Vec3 camFor = VectorOperations.normalise(new Vec3(1, 0, 0.05f));
     private static final Vec3 camUp = new Vec3(0.0f, 1.0f, 0.0f);
     private static final Vec3 camRight = VectorOperations.normalise(VectorOperations.cross(camFor, camUp));
-    private static final float fov = 20.0f;
+    private static final float fov = 70.0f;
 
     public static void main(String[] args) {
         //Initalise and create window
@@ -48,10 +50,12 @@ public class Main {
         GL43.glTexStorage2D(GL43.GL_TEXTURE_2D, 1, GL43.GL_RGBA32F, WIDTH, HEIGHT);
 
         //Get the scene data
-        int numObjects = 490;
+        //int numObjects = 490;
+        int numObjects = 244;
         ByteBuffer buffer = MemoryUtil.memAlloc(numObjects * 80);  //int and floats are 4 bytes
 
-        createScene(buffer);
+        //createTestScene(buffer);
+        createSnowglobeScene(buffer);
 
         buffer.flip();
 
@@ -74,7 +78,9 @@ public class Main {
         GL43.glUniform3f(GL43.glGetUniformLocation(program, "u_CamRight"), camRight.x(), camRight.y(), camRight.z());
         GL43.glUniform3f(GL43.glGetUniformLocation(program, "u_CamUp"), camUp.x(), camUp.y(), camUp.z());
         GL43.glUniform1f(GL43.glGetUniformLocation(program, "u_Fov"), fov);
-        GL43.glUniform3f(GL43.glGetUniformLocation(program, "u_backgroundCol"), 0, 0, 0);
+        //GL43.glUniform3f(GL43.glGetUniformLocation(program, "u_backgroundCol"), 0, 0, 0);
+        GL43.glUniform3f(GL43.glGetUniformLocation(program, "u_backgroundCol"), 0.02f, 0.15f, 0.3f);
+        //GL43.glUniform3f(GL43.glGetUniformLocation(program, "u_backgroundCol"), 1, 1, 1);
 
         System.out.println("Rendering " + WIDTH + "x" + HEIGHT + " image at " + SAMPLES_PER_PIXEL + " samples per pixel on GPU...");
         long startTime = System.currentTimeMillis();
@@ -155,7 +161,7 @@ public class Main {
         }
     }
 
-    private static void createScene(ByteBuffer buf){
+    private static void createTestScene(ByteBuffer buf){
 
         //addObject(buf, 0, 0, 0, 0,
         //        0.5f, 0.5f, 0.5f, 0,
@@ -291,5 +297,304 @@ public class Main {
 
 
         return program;
+    }
+
+    private static void createSnowglobeScene(ByteBuffer buf){
+
+        //Building
+        //floor
+        addObject(buf, 1, 0, 0, 0,
+                1, 1, 1, 0,
+                0, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 0, -1);
+
+        //left wall
+        addObject(buf, 1, 0, 0, 0,
+                0.66f, 0.66f, 0.66f, 0,
+                0, 0, 0, 0,
+                0.7f, 0, 1, 0,
+                0, 0, 0, 1);
+
+        //right wall
+        addObject(buf, 1, 0, 0, 0,
+                0.66f, 0.66f, 0.66f, 0,
+                0, 0, 0, 0,
+                0.7f, 0, -1, 0,
+                0, 0, 0, 1);
+
+        //back glass wall
+        addObject(buf, 1, 2, 1.5f, 0,
+                1, 1, 1, 0,
+                0, 0, 0, 0,
+                -1, 0, 0, 0,
+                0, 0, 0, 10);
+
+        //back blocking wall
+        addObject(buf, 0, 0, 0, 0,
+                1, 1, 1, 0,
+                0, 0, 0, 0,
+                1, 0, 0, 0,
+                0, 0, 0, 6);
+
+        //roof disk
+        addObject(buf, 11, 0, 0, 0,
+                0.85f, 0.85f, 0.85f, 0,
+                0, 4, 0, 0,
+                0, -1, 0, 0,
+                10, 0, 0, 10);
+
+        //joining support cylinders
+        addObject(buf, 2, 0, 0, 0,
+                0.5f, 0.5f, 0.5f, 0,
+                10, 0, 8, 0,
+                0, 1, 0, 0,
+                1, 20, 0, 1);
+
+        addObject(buf, 2, 0, 0, 0,
+                0.5f, 0.5f, 0.5f, 0,
+                10, 0, -8f, 0,
+                0, 1, 0, 0,
+                1, 20, 0, 1);
+
+        addObject(buf, 2, 0, 0, 0,
+                0.5f, 0.5f, 0.5f, 0,
+                10, 1, 0f, 0,
+                0, 0, 1, 0,
+                1, 20, 0, 1);
+
+        //light
+        addObject(buf, 0, 3, 0, 0,
+                10f, 10, 10, 0,
+                0f, 5, 0f, 0,
+                0, 0, 0, 1,
+                1, 0, 0, 0);
+
+        //lightshade
+        addObject(buf, 2, 0, 0, 0,
+                0.5f, 0.5f, 0.9f, 0,
+                0, 5, 0f, 0,
+                0, 1, 0, 0,
+                1.5f, 3, 0, 0);
+
+        //Table
+        addObject(buf, 2, 0, 0, 0,
+                0.6f, 0.3f, 0, 0,
+                0, 1.8f, 0, 0,
+                0, 1, 0, 0,
+                0.4f, 0.3f, 0, 1);
+
+        //globe
+        addObject(buf, 0, 2, 1.5f, 0,
+                1, 1, 1, 0,
+                0.3f, 2.049f, 0f, 0 ,
+                0, 0, 0, 0,
+                0.1f, 0, 0, 0);
+
+
+        //globe support
+        addObject(buf, 3, 1, 0.1f, 0,
+                1, 0.8f, 0, 0,
+                0.3f, 2.2f, 0, 0,
+                0, -1, 0, 0,
+                (float)Math.PI/12, 0.25f, 0.23f, 0);
+
+        //globe light
+        addObject(buf, 11, 3, 0, 0,
+                4, 4, 4, 0,
+                0.3f, 1.97f, 0, 0,
+                0, 1, 0, 1,
+                0.05f, 0, 0, -1.97f);
+
+        //snowman
+        addObject(buf, 0, 0, 0, 0,
+                0.92f, 0.94f, 0.96f, 0,
+                0.3f, 2.053f, 0, 0,
+                0, 0, 0, 0,
+                0.012f, 0, 0, 0);
+
+        addObject(buf, 0, 0, 0, 0,
+                0.92f, 0.94f, 0.96f, 0,
+                0.3f, 2.03f, 0, 0,
+                0, 0, 0, 0,
+                0.015f, 0, 0, 0);
+
+        addObject(buf, 0, 0, 0, 0,
+                0.92f, 0.94f, 0.96f, 0,
+                0.3f, 2f, 0, 0,
+                0, 0, 0, 0,
+                0.02f, 0, 0, 0);
+
+        //hat
+        addObject(buf, 2, 0, 0, 0,
+                0.1f, 0.1f, 0.1f, 0,
+                0.3f, 2.06f, 0, 0,
+                0, 1, 0, 0,
+                0.015f, 0.001f, 0, 1);
+        addObject(buf, 2, 0, 0, 0,
+                0.1f, 0.1f, 0.1f, 0,
+                0.3f, 2.0612f, 0, 0,
+                0, 1, 0, 0,
+                0.01f, 0.014f, 0, 1);
+        addObject(buf, 2, 0, 0, 0,
+                0.1f, 0.1f, 0.1f, 0,
+                0.3f, 2.0629f, 0, 0,
+                0, 1, 0, 0,
+                0.01f, 0.02f, 0, 1);
+
+        //snowman details
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.289f, 2.056f, 0, 0,
+                0, 0, 0, 0,
+                0.001f, 0, 0, 0);
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.295f, 2.056f, -0.01f, 0,
+                0, 0, 0, 0,
+                0.001f, 0, 0, 0);
+
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.289f, 2.051f, 0, 0,
+                0, 0, 0, 0,
+                0.001f, 0, 0, 0);
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.295f, 2.051f, -0.01f, 0,
+                0, 0, 0, 0,
+                0.001f, 0, 0, 0);
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.2932f, 2.049f, -0.008f, 0,
+                0, 0, 0, 0,
+                0.001f, 0, 0, 0);
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.2898f, 2.049f, -0.0025f, 0,
+                0, 0, 0, 0,
+                0.001f, 0, 0, 0);
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.2914f, 2.048f, -0.00515f, 0,
+                0, 0, 0, 0,
+                0.001f, 0, 0, 0);
+
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.2895f, 2.035f, -0.008f, 0,
+                0, 0, 0, 0,
+                0.0012f, 0, 0, 0);
+        addObject(buf, 0, 0, 0, 0,
+                0.02f, 0.02f, 0.02f, 0,
+                0.2895f, 2.025f, -0.008f, 0,
+                0, 0, 0, 0,
+                0.0012f, 0, 0, 0);
+
+        addObject(buf, 2, 0, 0, 0,
+                0.1f, 0.1f, 0.1f, 0,
+                0.31f, 2.038f, -0.018f, 0,
+                0.3f, 0.8f, -0.5f, 0,
+                0.0003f, 0.02f, 0, 1);
+        addObject(buf, 2, 0, 0, 0,
+                0.1f, 0.1f, 0.1f, 0,
+                0.31f, 2.038f, 0.028f, 0,
+                -0.3f, 0.8f, 0.5f, 0,
+                0.0003f, 0.02f, 0, 1);
+
+        addObject(buf, 3, 0, 0, 0,
+                0.926f, 0.566f, 0.129f, 0,
+                0.284f, 2.052f, -0.008f, 0,
+                1, 0, 0.5f, 0,
+                (float)Math.PI/24, 0.01f, 0, 0);
+
+        //snowflakes
+        for (int i = 0; i < 200; i++){
+            float x = (float)Math.random()/10;
+            float y = (float)Math.random()/10;
+            float z = (float)Math.random()/10;
+            if(x*x + y*y + z*z > 0.01){
+                i--;
+                continue;
+            }
+            if(Math.random() > 0.5){
+                x = -x;
+            }
+            if(Math.random() > 0.5){
+                y = -y;
+            }
+            if(Math.random() > 0.5){
+                z = -z;
+            }
+            float nx = (float)Math.random();
+            float ny = (float)Math.random();
+            float nz = (float)Math.random();
+            addObject(buf, 11, 0, 0, 0,
+                    0.92f, 0.94f, 0.96f, 0,
+                    x + 0.3f, y + 2.049f, z, 0,
+                    nx, ny, nz, 0,
+                    0.001f, 0, 0, -((x + 0.3f)*nx+(y + 2.049f)*ny+z*nz));
+        }
+
+        addObject(buf, 2, 1, 0.05f, 0,
+                0.6f, 0.5f, 0.3f, 0,
+                0.1f, 1.96f, -0.28f, 0,
+                -0.6f, 0, -0.3f, 0,
+                0.01f, 0.02f, 0, 0);
+        addObject(buf, 2, 1, 0f, 0,
+                0.4f, 0.5f, 0.7f, 0,
+                0.1f, 1.952f, -0.2f, 0,
+                -0.7f, 0, -0.5f, 0,
+                0.002f, 0.025f, 0, 0);
+        addObject(buf, 2, 1, 0.25f, 0,
+                0.4f, 0.6f, 0.1f, 0,
+                0.13f, 1.955f, -0.25f, 0,
+                -0.1f, 0, 0.3f, 0,
+                0.005f, 0.02f, 0, 0);
+        addObject(buf, 2, 2, 2f, 0,
+                1f, 1f, 1f, 0,
+                0.13f, 1.955f, -0.14f, 0,
+                0, 1, 0, 0,
+                0.015f, 0.1f, 0, 1);
+
+        addObject(buf, 0, 0, 0, 0,
+                0.2f, 0.4f, 0.6f, 0,
+                0.13f, 1.965f, -0.05f, 0,
+                0, 0, 0, 0,
+                0.015f, 0, 0, 0);
+        addObject(buf, 0, 1, 2f, 0,
+                0.7f, 0.6f, 0.5f, 0,
+                0.16f, 1.96f, -0.11f, 0,
+                0, 0, 0, 0,
+                0.01f, 0, 0, 0);
+        addObject(buf, 0, 1, 0, 0,
+                0.7f, 0.5f, 0.4f, 0,
+                0.24f, 1.97f, -0.2f, 0,
+                0, 0, 0, 0,
+                0.02f, 0, 0, 0);
+
+        //torch
+        addObject(buf, 2, 2, 0.2f, 0,
+                0.3f, 0.3f, 0.4f, 0,
+                0.2f, 1.962f, -0.29f, 0,
+                0.35f, 0, -0.4f, 0,
+                0.012f, 0.05f, 0, 1);
+        addObject(buf, 0, 2, 0.2f, 0,
+                0.3f, 0.3f, 0.4f, 0,
+                0.183537f, 1.962f, -0.271186f, 0,
+                0, 0, 0, 0,
+                0.012f, 0, 0, 0);
+        addObject(buf, 11, 3, 0, 0,
+                10, 10, 10, 0,
+                0.216528f, 1.962f, -0.30889f, 0,
+                0.35f, 0, -0.4f, 1,
+                0.012f, 0.05f, 0, 0.1993408f);
+        addObject(buf, 0, 2, 0.2f, 0,
+                0.3f, 0.3f, 0.4f, 0,
+                0.214617f, 1.970485f, -0.29382f, 0,
+                0, 0, 0, 0,
+                0.002f, 0, 0, 0);
+
+        //outside
     }
 }
